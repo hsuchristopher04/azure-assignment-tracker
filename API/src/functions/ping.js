59feb4a475ed1c2e9052159e7a9c454ac263c6af
Christopher Hsu <153141100+hsuchristopher04@users.noreply.github.com>
2026-04-12 22:50:20 -0400

    Create ping.js

---
+++ API/src/functions/ping.js
@@ -0,0 +1,13 @@
+const { app } = require('@azure/functions')
+
+app.http('ping', {
+  methods: ['GET'],
+  authLevel: 'anonymous',
+  route: 'ping',
+  handler: async () => {
+    return {
+      status: 200,
+      jsonBody: { ok: true, message: 'API is alive' }
+    }
+  }
+})
